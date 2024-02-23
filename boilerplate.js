4a0ae167090fb2bd77747abed5220d5822891df0
yurism <mascarenhasyuri@hotmail.com>
2024-02-23 08:03:01 -0300

    [Add] Canvas and background template

--- boilerplate.js
+++ boilerplate.js
@@ -26,9 +26,11 @@ function preload() {
 }
 function setup() {
     // Code in this function runs once before draw().
+    createCanvas(500, 500);
 }
 function draw() {
     // Code in this function runs every frame.
+    background(200);
 }
 `;
 
